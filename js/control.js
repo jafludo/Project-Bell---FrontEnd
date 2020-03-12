be24eedf9305a9f31480a281a8796c31b71a6bb5
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-03-12 16:36:40 +0100

    Add notation

--- js/control.js
+++ js/control.js
@@ -1,6 +1,24 @@
+$(document).ready(function() {
+    var recup = $("#button-addon1").val();
+    var recup2 = $("#button-addon2").val();
 
-alert("d");
-$("#submit").click(function(){
 
-    alert("evenement de click");
+    $("#submit").click(function(){
+        console.log("f");
+        
+    });
+
+    $("#button-addon1").click(function(){
+        recup++;
+        $("#button-addon1").val(recup);
+    });
+
+
 });
+
+
+class Request{
+
+}
+
+
