33be85306bba63caa50dc583def37ae5d849c90a
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-03-12 23:05:21 +0100

    add bell with form without notation

--- js/control.js
+++ js/control.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
     var recup = $("#button-addon1").val();
     var recup2 = $("#button-addon2").val();
-
+    var divinteger = 0;
+    var divid = "div0";
 
     $("#submit").click(function(){
-        var div = $(<div></div>);
-        $(div).append(<table></table>);
-        
+        $('<div/>', {
+            id: divid,
+            "class": "divson",
+        }).appendTo('#globaldiv');
+
+        $('<iframe />', {
+            name: 'frame1',
+            id: 'frame1',
+            frameborder: "0", 
+            src: 'https://www.youtube.com/embed/RYjfe8OSRFw'
+        }).appendTo("#"+divid);
+
+        $('<div/>', {
+            class: "notation d-flex"
+        }).appendTo("#"+divid);
+
+        divinteger++;
+        divid = "div"+divinteger;
+
     });
 
     /*
